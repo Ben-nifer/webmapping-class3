38c9c519c2aa543a6fefd54261f386687e086fca
Ben-nifer <bistman12@gmail.com>
2022-02-10 18:15:54 -0500

    New album data & consolidated listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,9 @@
-$('#adele').on('click', function() {
-	$(".img-container").html("<img src='https://m.media-amazon.com/images/I/71Ln3JLWyOL._SL1500_.jpg'/>");
-})
 
-$('#sound_of_music').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/7/7a/The-Sound-of-Music-CD.jpg'/>");
-})
 
-$('#michael_jackson').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png'/>");
-})
+// Consolidated listeners for all albums
+$('.artist-container').on('click', function(){
+  var albumArt = $(this).data('album-art')
+  $(".right-half").html(`<img src='${albumArt}'/>`);
+  // $(".right-half").html(`<img src=\'' + albumArt + albumYear + albumPeak '\''/>`);
 
-$('#tay').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/8/86/Taylor_Swift_-_Fearless.png'/>");
-})
-
-$('#bruce_springsteen').on('click', function() {
-	$(".right-half").html("<img src='https://m.media-amazon.com/images/I/819qdTNLMxL._SL1500_.jpg'/>");
-})
-
-$('#garth_brooks').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/5/5a/Garth_Brooks-Ropin%27_the_Wind_%28album_cover%29.jpg'/>");
-})
-
-$('#alanis_morissette').on('click', function() {
-	$(".right-half").html("<img src='https://m.media-amazon.com/images/I/81UsU658UVL._SL1425_.jpg'/>");
-})
-
-$('#michael_jackson').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png'/>");
-})
-
-$('#doctor_zhivago').on('click', function() {
-	$(".right-half").html("<img src='https://img.discogs.com/LBD-0mYc4ZQQofKqlqaK9GTJxBI=/fit-in/600x586/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6633676-1475849958-5055.jpeg.jpg'/>");
-})
-
-$('#nickelback').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/AllTheRightReasons.jpg/220px-AllTheRightReasons.jpg'/>");
-})
-
-$('#carole_king').on('click', function() {
-	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/5/52/Carole_King_-_Tapestry.jpg'/>");
 })
